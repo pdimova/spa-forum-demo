cb0e90aa68b09eb994b6bc6caf62be3679d2c68b
pollx <dimova.paola@gmail.com>
2017-05-05 21:30:45 +0300

    Express server running, mongodb connection established

---
+++ app.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express();
+//app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./lib/config/mongoose');
+
+require('./lib/routes/index')(app);
+
+
+app.use((req, res) => {
+    res.status(404).send('Oppps');
+});
+app.use((err, req, res, next) => {
+    console.error(err);
+});
+
+app.listen(3000, () => console.log('App running..'));
