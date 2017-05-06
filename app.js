3937bf0ddcc9b79aad2a59d4ba19062e27efc119
pollx <dimova.paola@gmail.com>
2017-05-06 12:22:56 +0300

    Token-based authentication added

--- app.js
+++ app.js
@@ -4,8 +4,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const config = require('./lib/config/config');
 const app = express();
-//app.use(morgan('dev'));
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -21,4 +23,4 @@ app.use((err, req, res, next) => {
     console.error(err);
 });
 
-app.listen(3000, () => console.log('App running..'));
+app.listen(config.port, () => console.log('Magic happens at http://localhost:' + config.port));
