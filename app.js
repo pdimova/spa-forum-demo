b7e4e2b58dd32de2c1df0e0ba470316c198f6857
pollx <dimova.paola@gmail.com>
2017-05-08 19:43:05 +0300

    Configuration updated

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 /* eslint-disable no-console */
 'use strict';
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
 const config = require('./lib/config/config');
-const app = express();
+const app = require('./lib/config/express')(config);
 
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-require('./lib/config/mongoose');
-
+require('./lib/config/mongoose')(config);
 require('./lib/routes/index')(app);
-
 
 app.use((req, res) => {
     res.status(404).send('Oppps');
 });
+
 app.use((err, req, res, next) => {
     console.error('Error handler middelware ' + err);
     
